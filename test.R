c939b38c1a2edd2113dd80555d1361425f49ce17
econmtlin <econ.mtlin@gmail.com>
2024-11-08 10:21:28 +0000

    Write something here, then click commit

--- test.R
+++ test.R
@@ -1,1 +1,13 @@
-test <- 1
+survey <- read_csv("https://raw.githubusercontent.com/tpemartin/113-1-R/refs/heads/main/Lecture-notes/ee-survey.csv")
+
+glimpse(survey)
+
+library(lubridate)
+
+# parse date time
+survey <- survey %>%
+  mutate(
+    `Timestamp` = mdy_hms(`Timestamp`, tz="Asia/Taipei")
+  )
+
+glimpse(survey)
