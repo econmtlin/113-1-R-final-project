eaa652f833a982242e4cf5f3f6c9782551b419e0
econmtlin <econ.mtlin@gmail.com>
2024-11-09 08:37:35 +0000

    research topics search

---
+++ data-search.R
@@ -0,0 +1,5 @@
+
+openDataSearchUrl <- "https://data.gov.tw/datasets/search?p=1&size=10&s=_score_desc&rft=%E8%87%BA%E4%B8%AD%E5%B8%82%E5%B0%B1%E6%A5%AD%E6%9C%8D%E5%8B%99%E4%B9%8B%E6%B1%82%E8%81%B7%E3%80%81%E6%B1%82%E6%89%8D%E5%8F%8A%E6%8E%A8%E4%BB%8B%E5%B0%B1"
+
+# 台中市職場人力吃緊度分析(Job market tightness analysis) -----
+taichungJobMarket <- read_csv("2015_01_1343-08-03-2_臺中市就業服務之求職、求才及推介就業人數－按職業分.csv")
