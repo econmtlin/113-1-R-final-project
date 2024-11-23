67f2ebfbf0c7a1f41d4aac05b05c572db79d5dba
econmtlin <econ.mtlin@gmail.com>
2024-11-23 08:26:07 +0000

    拆解地址

--- baby-data-import.R
+++ baby-data-import.R
@@ -6,5 +6,30 @@ glimpse(baby)
 
 baby <- baby |>
   mutate(
-    district=str_sub(地址,4,6)
+    district=str_sub(地址,4,6),
+    remaining_address = str_sub(地址,7)
+  ) 
+
+baby <- baby |>
+  mutate(
+    subdivision1 = str_extract(remaining_address,
+                               "[\u4e00-\u9fff]{2,3}里"),
+    remaining_address = str_remove(remaining_address,
+                                   "[\u4e00-\u9fff]{2,3}里")
+  )
+
+baby <- baby |>
+  mutate(
+    subdivision2 = str_extract(remaining_address,
+                               ".{2,3}鄰"),
+    remaining_address = str_remove(remaining_address,
+                                   ".{2,3}鄰")
+  )
+
+baby <- baby |>
+  mutate(
+    road = str_extract(remaining_address,
+                       "[\u4e00-\u9fff]{2,3}路"),
+    remaining_address = str_remove(remaining_address,
+                                   "[\u4e00-\u9fff]{2,3}路")
   )
